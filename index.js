8504d177efdfc653bb3c079a30a1dc358b51f1e3
Antonio Brandao <antoniobrandaodesign@gmail.com>
2015-02-12 23:30:57 +0000

    Added remove file extension method

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ module.exports = {
     {
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     	return re.test(string);
+    },
+
+    removeFileExtension: function(string)
+    {
+        var re = /(.*)\.[^.]+$/;
+    	return re.test(string);
     }
 }
