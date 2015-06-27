f9c9610ab9e1ddab5bc3b8e5641aa06581724ed4
Antonio Brandao <antoniobrandaodesign@gmail.com>
2015-06-27 02:07:30 +0100

    Changed to protoype technique

--- index.js
+++ index.js
@@ -4,24 +4,21 @@ String.prototype.capitalize = function(normalise) {
     return (normalise ? this.toLowerCase() : this).replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
 };
 
-module.exports = {
+String.prototype.isEmail = function()
+{
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(this);
+},
 
-    isEmail: function(string)
-    {
-        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    	return re.test(string);
-    },
+String.prototype.removeFileExtension = function()
+{
+    var re = /(.*)\.[^.]+$/;
+    return re.test(this);
+}
 
-    removeFileExtension: function(string)
-    {
-        var re = /(.*)\.[^.]+$/;
-        return re.test(string);
-    }
-
-    capitalizeEachWord: function(string)
-    {
-        return str.replace(/\w\S*/g, function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        });
-    }
+String.prototype.capitalizeEachWord = function()
+{
+    return this.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
 }
