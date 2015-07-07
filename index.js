60f67dd53f5a77d7cd598dc84fc8f5b5f9758052
Antonio Brandao <antoniobrandaodesign@gmail.com>
2015-07-07 12:55:56 +0100

    Added method Contains

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ String.prototype.capitalize = function(normalise) {
     return (normalise ? this.toLowerCase() : this).replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
 };
 
+String.prototype.contains = function(string) {
+    return this.indexOf(string) > -1;
+};
+
 String.prototype.isEmail = function()
 {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
